4ae3d6e5ab3307d739a1f76632e9af83a3434588
Muhammad Muhaimin <muhammad.r.muhaimin@gmail.com>
2019-05-02 20:57:06 -0700

    Add readme.md

---
+++ readme.md
@@ -0,0 +1,7 @@
+#### Prerequisite
+- Install conda
+- Create a new environment
+- Remove installation of scikit learn from that instance
+
+#### Setup
+- Run the script `setup.sh`
--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@ conda install -c anaconda jupyter -y
 conda install -c conda-forge pydotplus -y
 conda install -c anaconda graphviz -y
 conda install -c conda-forge matplotlib -y
-git clone git@github.com:scikit-learn/scikit-learn.git
 brew install libomp
 export CC=/usr/bin/clang
 export CXX=/usr/bin/clang++
@@ -14,6 +13,7 @@ export CFLAGS="$CFLAGS -I/usr/local/opt/libomp/include"
 export CXXFLAGS="$CXXFLAGS -I/usr/local/opt/libomp/include"
 export LDFLAGS="$LDFLAGS -L/usr/local/opt/libomp/lib -lomp"
 export DYLD_LIBRARY_PATH=/usr/local/opt/libomp/lib
+git clone git@github.com:mdrmuhaimin/scikit-custom-tree-spliter.git
 cd scikit-learn
 pip install .
 
